26bd5d88255e9107f8eaa56a230547ccb93f2c62
Alessandro <alessandroretuerto@hotmail.com>
2026-08-06 18:43:44 -0500

    Feat:Add new item for data productos

---
+++ catalog/src/data/data.js
@@ -0,0 +1,44 @@
+export const products = [
+  {
+    id: 1,
+    name: "Zenith Laptop Pro",
+    price: 3000,
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTt3jg8w279JUPItVwCPH_Q-Nr9o7oIDQEzlu-QZWIpQ&s=10",
+  },
+  {
+    id: 2,
+    name: "Precision Gaming Mouse",
+    price: 80,
+    imageUrl:
+      "https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7?q=80&w=500&auto=format&fit=crop",
+  },
+  {
+    id: 3,
+    name: "Tactile Mechanical Keyboard",
+    price: 200,
+    imageUrl:
+      "https://images.unsplash.com/photo-1595225476474-87563907a212?q=80&w=500&auto=format&fit=crop",
+  },
+  {
+    id: 4,
+    name: "Ultra HD 4K Monitor",
+    price: 900,
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdFefgj7kYbiIvbE4VaaqT95KnZJcHmKBXs2NHYZPwyQ&s=10",
+  },
+  {
+    id: 5,
+    name: "Xbox Series S",
+    price: 1200,
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsHwdgiSWuFtlYOSTtyZDAOsqcPf8Q9vGweVoQB0piew&s",
+  },
+  {
+    id: 6,
+    name: "PlayStation",
+    price: 2500,
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRnHxSPX_tpJHEt4EmsjlBwgLlx7e77bjRv9losv9f2uAoswv06zFWJWQ__udWyHjsYn2G8bv8N_EoPIudlf60KiJdlzoYVB7_YnBSyIHP6IURrHAStnLusHf7p-t5ANhCttK6XsQ",
+  },
+];
