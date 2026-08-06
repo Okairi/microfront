34eb6eac645adab4658afbcf874d2002e4451dbd
Alessandro <alessandroretuerto@hotmail.com>
2026-08-06 18:55:25 -0500

    feat:Add styles for estructure

--- cart/src/App.jsx
+++ cart/src/App.jsx
@@ -6,51 +6,87 @@ function App() {
 
   useEffect(() => {
     function handleCart(event) {
-      setProducts((prev) => [...prev, event.detail]);
+      const product = event.detail;
+
+      setProducts((prev) => {
+        const exists = prev.find((item) => item.id === product.id);
+
+        if (exists) {
+          return prev.map((item) =>
+            item.id === product.id
+              ? {
+                  ...item,
+                  quantity: item.quantity + 1,
+                }
+              : item,
+          );
+        }
+
+        return [
+          ...prev,
+
+          {
+            ...product,
+            quantity: 1,
+          },
+        ];
+      });
     }
 
-    window.addEventListener(
-      "add-to-cart",
-
-      handleCart,
-    );
+    window.addEventListener("add-to-cart", handleCart);
 
     return () => {
-      window.removeEventListener(
-        "add-to-cart",
-
-        handleCart,
-      );
+      window.removeEventListener("add-to-cart", handleCart);
     };
   }, []);
 
   const total = products.reduce(
-    (sum, item) => sum + item.price,
+    (sum, item) => sum + item.price * item.quantity,
+
+    0,
+  );
+
+  const totalProducts = products.reduce(
+    (sum, item) => sum + item.quantity,
 
     0,
   );
 
   return (
     <div className="cart">
-      <h2>🛒 Carrito</h2>
+      <h2 className="titleCart">Tú carrito de compras</h2>
 
       {products.length === 0 ? (
-        <p>No hay productos</p>
+        <p className="cart-empty">Tu carrito está vacío</p>
       ) : (
-        products.map((item, index) => (
-          <div className="cart-item" key={index}>
-            <span>{item.name}</span>
+        products.map((item) => (
+          <div className="cart-item" key={item.id}>
+            <div>
+              <strong>{item.name}</strong>
 
-            <span>S/. {item.price}</span>
+              <p>Cantidad: {item.quantity}</p>
+            </div>
+
+            <span>S/. {item.price * item.quantity}</span>
           </div>
         ))
       )}
 
       <hr />
 
-      <h3>Total: S/. {total}</h3>
+      <div className="cart-total">
+        <span>Productos:</span>
 
-      <button>Comprar</button>
+        <span>{totalProducts}</span>
+      </div>
+
+      <div className="cart-total">
+        <span>Total:</span>
+
+        <span>S/. {total}</span>
+      </div>
+
+      <button disabled={products.length === 0}>Comprar</button>
     </div>
   );
 }
